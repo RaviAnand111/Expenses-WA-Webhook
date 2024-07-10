f91b30065d5ee20908d2a5b73ef9714c18b398fe
Ravi Anand <ravi@sploot.tech>
2024-07-10 22:48:45 +0530

    wa webhook setup

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv")
+
+// configuring dotenv
+dotenv.config()
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("listening on port");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("GET working");
+});
+
+app.post("/webhook", (req, res) => {
+  const body_param = req.body;
+
+  console.log(JSON.stringify(body_param), "body");
+
+  const { entry } = body_param;
+  const { value } = entry[0]?.changes[0];
+  const { metadata, messaging_product, messages } = value;
+
+  if (value && messages) {
+    const phoneNumberID = metadata?.phone_number_id;
+    const from = messages[0]?.from;
+    const textBody = messages[0]?.text?.body;
+
+    let body = {
+      messaging_product: messaging_product,
+      to: from,
+      type: "template",
+      text: {
+        body: "Hi,this is daily check in expenses",
+      },
+      /*template: {
+        name: "hello world",
+        language: { code: "en_US" },
+      },
+      */
+    };
+
+    const headers = {
+      Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+      "Content-Type": "application/json",
+    };
+
+    axios.post(
+      `${process.env.META_BASE_URL}/${API_VERSION}/${phoneNumberID}/messages`,
+      body,
+      { headers },
+    );
+
+    res.status(200).send("reply sent");
+  } else {
+    res.status(404).send("No response");
+  }
+});
